5de20185430c9e9b6476fe02664a225a1d136c0f
Nicholas Ionata <n.ionata129@gmail.com>
2016-11-22 22:19:28 -0500

    Added API request to get quotes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
-    $("#new-quote-btn").on("click", function() {
-       var quote = "";
-       var author = "";
+function newQuote() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+       var quote = json.quoteText;
+       var author = json.quoteAuthor;
        $("#quote-text").text(quote);
        $("#quote-author").text(author);
        $("#new-tweet-btn").attr("href", "https://twitter.com/intent/tweet?text=" + "\"" + quote + "\" " + author);
     });
+};
+
+$(document).ready(function() {
+    newQuote();
+    $("#new-quote-btn").on("click", function() {
+        console.log("Sent");
+        newQuote();
+    });
 });
+
